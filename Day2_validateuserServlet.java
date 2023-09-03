2c96d9af7d71af10fd658e82ad3799bbea6768db
Mihir Patwardhan <114598789+Mihir-Patwardhan@users.noreply.github.com>
2023-09-03 18:58:10 +0530

    Add files via upload

---
+++ Day2_validateuserServlet.java
@@ -0,0 +1,58 @@
+package com.pages;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class validateuserServlet
+ */
+@WebServlet("/validateuser")
+public class validateuserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public validateuserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		String str1=request.getParameter("txtname");
+		String str2=request.getParameter("txtpwd");
+		
+		if(str1.equals("mihir") && str2.equals("mihir"))
+		{
+		pw.write("<h2>User Registered : "+str1);
+		}
+		else
+		{
+			response.sendRedirect("Register.html");
+			pw.write("Invalid Credentials");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
